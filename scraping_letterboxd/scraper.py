bba4d2953c6472e2810809079fba0fe439067f50
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-03-24 15:34:59 -0300

    feat: retry fetching movie data using year if first attempt fails

--- scraping_letterboxd/scraper.py
+++ scraping_letterboxd/scraper.py
@@ -15,62 +15,87 @@ logger = setup_logger(LOG_PATH)
 def setup_driver() -> webdriver.Chrome | None:
     try:
         options = webdriver.ChromeOptions()
-        options.add_argument("--headless") 
-        options.add_argument("--ignore-certificate-errors")  
-        options.add_argument("--disable-web-security")  
-        options.add_argument("--disable-gpu") 
-        options.add_argument("--no-sandbox") 
-        options.add_argument("--disable-dev-shm-usage") 
+        options.add_argument("--headless")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--disable-web-security")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
-        service = Service(ChromeDriverManager().install())  
-        driver = webdriver.Chrome(service=service, options=options) 
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
         return driver
     except Exception as e:
-        print(f"Error al iniciar el WebDriver: {e}")
-        return  
+        logger.error(f"Error al iniciar el WebDriver: {e}")
+        return 
 
-def get_movie_data(film_url, film_name):
+def get_movie_data(film_name, year):
+    """
+    Obtiene los datos de la película y descarga las imágenes.
+    Intenta la URL sin año primero. Si falla y el año está en el JSON, intenta la URL con año.
+    """
     driver = setup_driver()
-    driver.get(film_url)
-    time.sleep(3)
-    
-    try:
-        title = driver.find_element(By.CSS_SELECTOR, "h1.headline-1").text
-        sinopsis = driver.find_element(By.CSS_SELECTOR, "div.truncate p").text
-        year = int(driver.find_element(By.CSS_SELECTOR, "div.releaseyear a").text)
-        director = driver.find_element(By.CSS_SELECTOR, "a.text-slug").text
-        genero = driver.find_element(By.CSS_SELECTOR, "a[href*='/films/genre/']").text
-        actors = [elem.text for elem in driver.find_elements(By.CSS_SELECTOR, "a[href*='/actor/']")][:5]
-        cover_element = driver.find_element(By.CSS_SELECTOR, "section.poster-list a[data-js-trigger='postermodal']")
-        cover_url = cover_element.get_attribute("href")
-        backdrop_element = driver.find_element(By.ID, "backdrop")
-        poster_url = backdrop_element.get_attribute("data-backdrop")
-
-        poster_filename = f"{film_name}-poster.jpg"
-        poster_path = os.path.join(OUTPUT_POSTERS_PATH, poster_filename)
-        download_image(poster_url, poster_path)
-
-        driver.quit()
-
-        return {
-            "title": title,
-            "sinopsis": sinopsis,
-            "year": year,
-            "director": {"id": generate_id(director), "name": director},
-            "genero": {"id": generate_id(genero), "name": genero},
-            "cover": f"{CLIENT_COVERS_PATH}{film_name}-cover.jpg",
-            "was_watched": False,
-            "rating": None,
-            "actors": [{"id": generate_id(actor), "name": actor} for actor in actors],
-            "poster_url": f"{CLIENT_POSTERS_PATH}{film_name}-poster.jpg",  # Ruta local del poster descargado
-            "cover_url": cover_url
-        }
-    except Exception as e:
-        logger.error(f"Error obteniendo datos de {film_url}: {e}")
-        driver.quit()
+    if not driver:
         return None
 
+    film_urls = [f"{BASE_URL}{film_name}/"]
+    # Letterboxd a vece usa el formato nombre-pelicula-yyyy donde yyyy es el año de lanzamiento, si falla la primera vez se intenta con esta nueva manera
+    if year:  
+        film_urls.append(f"{BASE_URL}{film_name}-{year}/")
+
+    for index, film_url in enumerate(film_urls):
+        driver.get(film_url)
+        time.sleep(3)
+
+        try:
+            title = driver.find_element(By.CSS_SELECTOR, "h1.headline-1").text
+            sinopsis = driver.find_element(By.CSS_SELECTOR, "div.truncate p").text
+            year = int(driver.find_element(By.CSS_SELECTOR, "div.releaseyear a").text)
+            director = driver.find_element(By.CSS_SELECTOR, "a.text-slug").text
+            genero = driver.find_element(By.CSS_SELECTOR, "a[href*='/films/genre/']").text
+            actors = [elem.text for elem in driver.find_elements(By.CSS_SELECTOR, "a[href*='/actor/']")][:5]
+
+            cover_element = driver.find_element(By.CSS_SELECTOR, "section.poster-list a[data-js-trigger='postermodal']")
+            cover_url = cover_element.get_attribute("href")
+            backdrop_element = driver.find_element(By.ID, "backdrop")
+            poster_url = backdrop_element.get_attribute("data-backdrop")
+
+            cover_filename = f"{film_name}-cover.jpg"
+            cover_path = os.path.join(OUTPUT_COVERS_PATH, cover_filename)
+            download_image(cover_url, cover_path)
+
+            poster_filename = f"{film_name}-poster.jpg"
+            poster_path = os.path.join(OUTPUT_POSTERS_PATH, poster_filename)
+            download_image(poster_url, poster_path)
+
+            driver.quit()
+
+            return {
+                "title": title,
+                "sinopsis": sinopsis,
+                "year": year,
+                "director": {"id": generate_id(director), "name": director},
+                "genero": {"id": generate_id(genero), "name": genero},
+                "cover": f"{CLIENT_COVERS_PATH}{cover_filename}",
+                "was_watched": False,
+                "rating": None,
+                "actors": [{"id": generate_id(actor), "name": actor} for actor in actors],
+                "poster_url": f"{CLIENT_POSTERS_PATH}{poster_filename}"
+            }
+        except Exception as e:
+            logger.warning(f"Error obteniendo datos desde {film_url}: {e}")
+
+            # Si la primera búsqueda falló y no hay segunda URL, se corta el intento
+            if index == 0 and len(film_urls) == 1:
+                break
+
+    driver.quit()
+    return 
+
 def download_image(image_url, save_path):
+    """
+    Descarga una imagen desde una URL y la guarda en el sistema de archivos.
+    """
     try:
         response = requests.get(image_url, stream=True)
         if response.status_code == 200:
@@ -103,18 +128,13 @@ def main():
 
     for idx, movie in enumerate(movies, start=1):
         film_name = movie["title"].replace(" ", "-").lower()
-        film_url = f"{BASE_URL}{film_name}/"
-        
-        movie_data = get_movie_data(film_url, film_name)
+        year = movie.get("year", "")
+        movie_data = get_movie_data(film_name, year)
+
         if movie_data:
-            save_path = os.path.join(OUTPUT_COVERS_PATH, f"{film_name}-cover.jpg")
-            if download_image(movie_data["cover_url"], save_path):
-                movie_data["id"] = idx
-                del movie_data["cover_url"]
-                movie_data_list.append(movie_data)
-                success += 1
-            else:
-                failed_movies.append(film_name)
+            movie_data["id"] = idx
+            movie_data_list.append(movie_data)
+            success += 1
         else:
             failed_movies.append(film_name)
 
